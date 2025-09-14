304d2e0cfff2740153692437a0422fc13b512cbd
Aadi Jain <aadij4945@gmail.com>
2025-09-14 18:53:28 +0530

    Create src/test/java/hooks/Hooks.java

---
+++ src/test/java/hooks/Hooks.java
@@ -0,0 +1,16 @@
+package hooks;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+    @Before
+    public void setUp() {
+        // Add setup code here
+    }
+
+    @After
+    public void tearDown() {
+        // Add teardown code here
+    }
+}
